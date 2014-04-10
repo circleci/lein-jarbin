3794cac8a11e22ff6e9511a55810a1939b7fd401
Allen Rohner <arohner@gmail.com>
2014-04-10 14:40:18 -0500

    Bump the version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci/lein-jarbin "0.1.0"
+(defproject circleci/lein-jarbin "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
